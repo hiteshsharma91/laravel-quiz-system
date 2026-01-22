de6353aee7e706a6607ef47566d38fe20eebbe11
hiteshsharma91 <hiteshsit11@gmail.com>
2026-01-22 16:19:01 +0530

    create admin controller, admin.blade and database

--- resources/views/admin-login.blade.php
+++ resources/views/admin-login.blade.php
@@ -7,15 +7,16 @@
 <body class=' bg-gray-400 flex items-center justify-center min-h-screen' >
     <div class=' bg-white p-8 rounded-2xl shadow-lg w-full max-w-sm'>
         <h2 class=" text-2xl text-center text-gray-800 mb-6">Admin login</h2>
-        <form action="" method="post" class=" space-y-4">
+        <form action="admin-login" method="post" class=" space-y-4">
+            @csrf 
             <div>
                 <label for="" class=" text-gray-600 mb-1">Admin name</label>
-                <input type="text" placeholder="Enter Admin name"
+                <input type="text" name="name" placeholder="Enter Admin name"
                 class=" w-full px-4 py-2 border border-gray-300 rounded-xl focus:outline-none">
             </div>
             <div>
                 <label for="" class=" text-gray-600 mb-1">Password</label>
-                <input type="password" placeholder="Enter Admin password"
+                <input type="password" name="password" placeholder="Enter Admin password"
                 class=" w-full px-4 py-2 border border-gray-300 rounded-xl focus:outline-none">
             </div>
             <button type="submit" class="w-full px-4 py-2 text-white bg-green-400 rounded-xl">Login</button>
