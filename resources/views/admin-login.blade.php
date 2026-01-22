a5f71aa1e4ec04ad34146c6c0da61259a0c52296
hiteshsharma91 <hiteshsit11@gmail.com>
2026-01-22 15:37:57 +0530

    admin login page create

---
+++ resources/views/admin-login.blade.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Admin login</title>
+    @vite('resources/css/app.css')
+</head>
+<body class=' bg-gray-400 flex items-center justify-center min-h-screen' >
+    <div class=' bg-white p-8 rounded-2xl shadow-lg w-full max-w-sm'>
+        <h2 class=" text-2xl text-center text-gray-800 mb-6">Admin login</h2>
+        <form action="" method="post" class=" space-y-4">
+            <div>
+                <label for="" class=" text-gray-600 mb-1">Admin name</label>
+                <input type="text" placeholder="Enter Admin name"
+                class=" w-full px-4 py-2 border border-gray-300 rounded-xl focus:outline-none">
+            </div>
+            <div>
+                <label for="" class=" text-gray-600 mb-1">Password</label>
+                <input type="password" placeholder="Enter Admin password"
+                class=" w-full px-4 py-2 border border-gray-300 rounded-xl focus:outline-none">
+            </div>
+            <button type="submit" class="w-full px-4 py-2 text-white bg-green-400 rounded-xl">Login</button>
+        </form>
+
+    </div>
+
+
+</body>
+</html>
